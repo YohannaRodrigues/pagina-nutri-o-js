4da472ebe01a8c791f3af7f920898a2a4e6a7483
Yohanna Rodrigues <yohannarodrigues15@gmail.com>
2022-02-21 22:58:22 -0300

    mexendo no js

--- js/calculo-imc.js
+++ js/calculo-imc.js
@@ -3,47 +3,43 @@ var paciente = document.querySelectorAll(".paciente");
 console.log(paciente);
 
 for (var i = 0; i < paciente.length; i++) {
-  var paciente = pacientes[i];
+  var pacientes = paciente[i];
 
-  var tdPeso = paciente.querySelector(".info-peso");
+  var tdPeso = pacientes.querySelector(".info-peso");
   var peso = tdPeso.textContent;
 
-  console.log(peso);
-
-  var tdAltura = paciente.querySelector(".info-altura");
+  var tdAltura = pacientes.querySelector(".info-altura");
   var altura = tdAltura.textContent;
 
-  console.log(altura);
-
-  var tdImc = paciente.querySelector(".info-imc");
-
-  console.log(tdImc);
+  var tdImc = pacientes.querySelector(".info-imc");
 
   var pesoValido = true;
   var alturaValida = true;
-
-  console.log(pesoValido);
-  console.log(alturaValida);
 
   if (peso <= 0 || peso >= 300) {
     console.log("Peso invalido, verifique suas informações!");
     pesoValido = false;
     tdImc.textContent = "Peso invalido";
-    paciente.style.color = "red";
+    pacientes.classList.add("paciente-invalido");
   }
-  console.log("oiiii");
 
   if (altura <= 0 || altura >= 3.0) {
     console.log("Altura invalida, verifique suas informações!");
     alturaValida = false;
     tdImc.textContent = "Altura invalido";
-    paciente.style.color = "lightcoral";
+    pacientes.classList.add("paciente-invalido");
   }
-  console.log("oiiii");
 
   if (alturaValida && pesoValido) {
-    var imc = peso / (altura * altura);
+    var imc = calculaIMC(peso, altura);
     tdImc.textContent = imc.toFixed(2);
   }
-  console.log("oiiii");
+}
+
+function calculaIMC(peso, altura) {
+  var imc = 0;
+
+  imc = peso / (altura * altura);
+
+  return imc;
 }
